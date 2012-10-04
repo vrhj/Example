a954ed5b491e23ab9a559617d0acdd9d45884e9b
jv <jvelilla@hexacta.com>
2012-10-04 15:32:41 -0300

    Adding test case

--- src/test/java/hx/atm/AccountTest.java
+++ src/test/java/hx/atm/AccountTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 public class AccountTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void create_a_new_account_balance_should_be_1000() {
+		Account account = new Account();
+		assertEquals("Expected balance 1000",1000,account.getBalance());
 	}
 
 }
