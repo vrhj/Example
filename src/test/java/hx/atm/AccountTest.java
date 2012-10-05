24fc45a342c834afc34a7933f0389daaf19042f4
jv <jvelilla@hexacta.com>
2012-10-05 12:31:37 -0300

    add deposit feature

--- src/test/java/hx/atm/AccountTest.java
+++ src/test/java/hx/atm/AccountTest.java
@@ -11,5 +11,13 @@ public class AccountTest {
 		Account account = new Account();
 		assertEquals("Expected balance 1000",1000,account.getBalance());
 	}
+	
+	
+	@Test
+	public void deposit_1000_should_return_current_balance_plus_1000() {
+		Account account = new Account();
+		account.deposit(1000);
+		assertEquals("Expected balance 2000",2000,account.getBalance());
+	}
 
 }
